37d2c181b111d36cb6b5f319d5a790e035e60aff
typefo <typefo@qq.com>
2017-06-11 18:12:48 +0800

    updated some code

--- www/app/models/System.php
+++ www/app/models/System.php
@@ -10,10 +10,251 @@ use Tool\Filter;
 
 class SystemModel {
 
-    public  $db = null;
+    public $db = null;
+    public $config = null;
 
     public function __construct($username) {
         $db = Yaf\Registry::get('db');
+        $config = Yaf\Registry::get('config');
+    }
+
+    public function regenAcl() {
+        $server = new ServerModel();
+        $result = $server->getAll();
+
+        if (count($result) > 0) {
+            $file = $this->config->fs->path . '/conf/acl.xml';
+
+            /* Check if the file is writable */
+            if (!is_writable($file)) {
+                error_log('Cannot write file ' . $file . ' permission denied');
+                return false;
+            }
+
+            $xml = '<list name="local" default="deny">' . "\n";
+            foreach ($result as $obj) {
+                $xml .= $obj['call'] == 1 ? '  <node type="allow" cidr="' . $obj['ip'] . '/32"/>' . "\n" : '';
+            }
+            $xml .= '</list>' . "\n";
+
+            $fp = fopen($file, "w");
+            if ($fp) {
+                fwrite($fp, $xml);
+                fclose($fp);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function regenPlan($rid = null) {
+        $route = new RouteModel();
+        $result = $route->get($rid);
+
+        if (count($result) > 0) {
+            $file = $this->config->fs->path . '/conf/dialplan/' . $result['name']. '.xml';
+            $dialplan = new DialplanModel();
+            $extensions = $dialplan->getAll($rid);
+
+            if (count($extensions) > 0) {
+                $xml = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+                $xml .= '<include>' . "\n";
+                $xml .= '  <context name="' . $result['name'] . '">' . "\n";
+                $xml .= '    <extension name="unloop">' . "\n";
+                $xml .= '      <condition field="${unroll_loops}" expression="^true$"/>' . "\n";
+                $xml .= '      <condition field="${sip_looped_call}" expression="^true$">' . "\n";
+                $xml .= '        <action application="deflect" data="${destination_number}"/>' . "\n";
+                $xml .= '      </condition>' . "\n";
+                $xml .= '    </extension>' . "\n\n";
+
+                foreach ($extensions as $obj) {
+                    $xml .= '    <extension name="' . $obj['id'] . '">' . "\n";
+
+                    $field = 'destination_number';
+                    switch ($obj['type']) {
+                        case 1:
+                            $field = 'caller_id_number';
+                            break;
+                        case 2:
+                            $field = 'destination_number';
+                            break;
+                        default:
+                            break;
+                    }
+
+                    $xml .= '      <condition field="' . $field . '" expression="' . $obj['rexp'] . '">' . "\n";
+                    $xml .= '        <action application="set" data="called=$1"/>' . "\n";
+                    $xml .= '        <action application="set" data="call_timeout=60"/>' . "\n";
+                    $xml .= '        <action application="set" data="ringback=${cn-ring}"/>' . "\n";
+                    $xml .= '        <action application="set" data="RECORD_STEREO=false"/>' . "\n";
+                    $xml .= '        <action application="set" data="RECORD_ANSWER_REQ=true"/>' . "\n";
+                    $xml .= '        <action application="record_session" data="/var/record/${strftime(%Y/%m/%d}/${caller_id_number}-${called}-${uuid}.wav"/>' . "\n";
+                    $xml .= '        <action application="bridge" data="sofia/' . $obj['sofia'] . '/${called}@' . $obj['server'] . '"/>' . "\n";
+                    $xml .= '        <action application="hangup"/>' . "\n";
+                    $xml .= '      </condition>' . "\n";
+                    $xml .= '    </extension>' . "\n\n";
+                }
+
+                $xml .= '  </context>' . "\n";
+                $xml .= '</include>' . "\n";
+
+                $fp = fopen($file, "w");
+                if ($fp) {
+                    fwrite($fp, $xml);
+                    fclose($fp);
+                    return true;
+                }
+
+                error_log('Cannot open file ' . $file . ' permission denied');
+            }
+        }
+
+        return false;
+    }
+
+    public function regenDefXml() {
+        $server = new ServerModel();
+        $result = $server->getAll();
+        if (count($result) > 0) {
+            $file = $this->config->fs->path . '/conf/dialplan/default.xml';
+
+            /* Check if the file is writable */
+            if (!is_writable($file)) {
+                error_log('Cannot write file ' . $file . ' permission denied');
+                return false;
+            }
+
+            $xml = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+            $xml .= '<include>' . "\n";
+            $xml .= '  <context name="' . $route['name'] . '">' . "\n";
+            $xml .= '    <extension name="unloop">' . "\n";
+            $xml .= '      <condition field="${unroll_loops}" expression="^true$"/>' . "\n";
+            $xml .= '      <condition field="${sip_looped_call}" expression="^true$">' . "\n";
+            $xml .= '        <action application="deflect" data="${destination_number}"/>' . "\n";
+            $xml .= '      </condition>' . "\n";
+            $xml .= '    </extension>' . "\n\n";
+
+            foreach ($result as $obj) {
+                $xml .= '    <extension name="' . $obj['ip'] . '">' . "\n";
+                $xml .= '      <condition field="network_addr" expression="^' . str_replace('.', '\.', $obj['ip']) . '$">' . "\n";
+                $xml .= '        <action application="transfer" data="${destination_number} XML ' . $obj['route'] . '"/>';
+                $xml .= '      </condition>' . "\n";
+                $xml .= '    </extension>' . "\n\n";
+            }
+
+            $xml .= '  </context>' . "\n";
+            $xml .= '</include>' . "\n";
+
+            /* Write configuration file */
+            $fp = fopen($file, "w");
+            if ($fp) {
+                fwrite($fp, $xml);
+                fclose($fp);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function regenSofia() {
+        $interface = new InterfaceModel();
+        $result = $interface->getAll();
+
+        if (count($result) > 0) {
+            foreach ($result as $obj) {
+                $xml =  '<profile name="' . $obj['name'] . '">' . "\n";
+                $xml .= '  <domains>' . "\n";
+                $xml .= '    <domain name="all" alias="false" parse="true"/>' . "\n";
+                $xml .= '  </domains>' . "\n";
+                $xml .= '  <settings>' . "\n";
+                $xml .= '    <param name="debug" value="0"/>' . "\n";
+                $xml .= '    <param name="sip-trace" value="no"/>' . "\n";
+                $xml .= '    <param name="sip-capture" value="no"/>' . "\n";
+                $xml .= '    <param name="rfc2833-pt" value="101"/>' . "\n";
+                $xml .= '    <param name="sip-port" value="' . $obj['port'] . '"/>' . "\n";
+                $xml .= '    <param name="dialplan" value="XML"/>' . "\n";
+                $xml .= '    <param name="context" value="default"/>' . "\n";
+                $xml .= '    <param name="dtmf-duration" value="2000"/>' . "\n";
+                $xml .= '    <param name="inbound-codec-prefs" value="' . $obj['in_code'] . '"/>' . "\n";
+                $xml .= '    <param name="outbound-codec-prefs" value="' . $obj['out_code'] . '"/>' . "\n";
+                $xml .= '    <param name="hold-music" value="$${hold_music}"/>' . "\n";
+                $xml .= '    <param name="rtp-timer-name" value="soft"/>' . "\n";
+                $xml .= '    <param name="local-network-acl" value="localnet.auto"/>' . "\n";
+                $xml .= '    <param name="manage-presence" value="false"/>' . "\n";
+                $xml .= '    <param name="apply-inbound-acl" value="local"/>' . "\n";
+                $xml .= '    <param name="inbound-codec-negotiation" value="generous"/>' . "\n";
+                $xml .= '    <param name="nonce-ttl" value="60"/>' . "\n";
+                $xml .= '    <param name="auth-calls" value="false"/>' . "\n";
+                $xml .= '    <param name="inbound-late-negotiation" value="true"/>' . "\n";
+                $xml .= '    <param name="inbound-zrtp-passthru" value="true"/>' . "\n";
+                $xml .= '    <param name="rtp-ip" value="' . $obj['ip'] . '"/>' . "\n";
+                $xml .= '    <param name="sip-ip" value="' . $obj['ip'] . '"/>' . "\n";
+                $xml .= '    <param name="ext-rtp-ip" value="auto-nat"/>' . "\n";
+                $xml .= '    <param name="ext-sip-ip" value="auto-nat"/>' . "\n";
+                $xml .= '    <param name="rtp-timeout-sec" value="300"/>' . "\n";
+                $xml .= '    <param name="rtp-hold-timeout-sec" value="1800"/>' . "\n";
+                $xml .= '    <param name="tls" value="false"/>' . "\n";
+                $xml .= '    <param name="tls-only" value="false"/>' . "\n";
+                $xml .= '    <param name="tls-bind-params" value="transport=tls"/>' . "\n";
+                $xml .= '    <param name="tls-sip-port" value="' . $obj['port'] . '"/>' . "\n";
+                $xml .= '    <param name="tls-passphrase" value=""/>' . "\n";
+                $xml .= '    <param name="tls-verify-date" value="true"/>' . "\n";
+                $xml .= '    <param name="tls-verify-policy" value="none"/>' . "\n";
+                $xml .= '    <param name="tls-verify-depth" value="2"/>' . "\n";
+                $xml .= '    <param name="tls-verify-in-subjects" value=""/>' . "\n";
+                $xml .= '    <param name="tls-version" value="tlsv1,tlsv1.1,tlsv1.2"/>' . "\n";
+                $xml .= '  </settings>' . "\n";
+                $xml .= '</profile>' . "\n";
+
+                $file = $this->config->fs->path . '/conf/sofia/' . $obj['name'] . '.xml';
+                $fp = fopen($file, "w");
+                if ($fp) {
+                   fwrite($fp, $xml);
+                   fclose($fp);
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function reloadXml() {
+        if ($this->eslCmd('bgapi reloadxml')) {
+            return true;
+        }
+        return false;
+    }
+
+    public function reloadAcl() {
+        if ($this->eslCmd('bgapi reloadacl')) {
+            return true;
+        }
+        return false;
+    }
+
+    public function eslCmd($cmd = null) {
+        if ($cmd && is_string($cmd)) {
+            $config = $this->config->esl;
+
+            /* Conection to freeswitch */
+            $esl = new ESLconnection($config->host, $config->port, $config->password);
+
+            if ($esl) {
+                /* Send command to freeswitch execute */
+                $esl->send($cmd);
+                /* Close esl connection */
+                $esl->disconnect();
+                return true;
+            }
+
+            error_log('esl cannot connect to freeswitch', 0);
+        }
+
+        return false;
     }
 
     public function sysInfo($esl = null) {
