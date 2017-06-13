1584b2660d141b747b92369ae2dc44e9ed0ba676
typefo <typefo@qq.com>
2017-06-13 13:44:58 +0800

    updated some files

--- www/app/controllers/Dialplan.php
+++ www/app/controllers/Dialplan.php
@@ -93,19 +93,10 @@ class DialplanController extends Yaf\Controller_Abstract {
 
     public function upAction() {
         $rid = $this->getRequest()->getQuery('rid');
-        $url = 'http://' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . '/dialplan?rid=' . $rid;
-        $response = $this->getResponse();
-        $response->setRedirect($url);
-        $response->response();
         return false;
     }
 
     public function downAction() {
-        $rid = $this->getRequest()->getQuery('rid');
-        $url = 'http://' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . '/dialplan?rid=' . $rid;
-        $response = $this->getResponse();
-        $response->setRedirect($url);
-        $response->response();
         return false;
     }
 }
