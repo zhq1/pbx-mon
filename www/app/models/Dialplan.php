d0ae189d3fb29cb1eaff9820a3c0c00b3053f990
typefo <typefo@qq.com>
2017-06-13 15:15:00 +0800

    updated some files

--- www/app/models/Dialplan.php
+++ www/app/models/Dialplan.php
@@ -223,7 +223,7 @@ class DialplanModel {
         if ($rid > 0) {
             $ben = $rid * 100;
             $end = ($rid + 1) * 100;
-            $sql = 'SELECT max(id) AS id FROM `' . $this->table '` WHERE id BETWEEN ' . $ben . ' AND ' . $end . ' ORDER BY id';
+            $sql = 'SELECT max(id) AS id FROM `' . $this->table . '` WHERE id BETWEEN ' . $ben . ' AND ' . $end . ' ORDER BY id';
             $result = $this->db->query($sql)->fetchAll();
             if ((count($result) > 0) && (intval($result['id']) >= $ben)) {
                 return intval($result['id']);
