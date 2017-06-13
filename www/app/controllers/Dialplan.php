b7934d52e278718b5e5e4a7f347d1c6b03f4f267
typefo <typefo@qq.com>
2017-06-13 11:57:23 +0800

    updated some files

--- www/app/controllers/Dialplan.php
+++ www/app/controllers/Dialplan.php
@@ -45,6 +45,8 @@ class DialplanController extends Yaf\Controller_Abstract {
         $route = new RouteModel();
         $this->getView()->assign('route', $route->get($rid));
         $this->getView()->assign("data", $data);
+        echo '=================================<br>'
+        var_dump($data);
         exit;
         return true;
 	}
