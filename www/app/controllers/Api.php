762fc7db778992dcdc6def6fbf3588037e386cb4
typefo <typefo@qq.com>
2017-06-15 14:59:20 +0800

    update templete

---
+++ www/app/controllers/Api.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * The Api Controller
+ * Link http://github.com/typefo/pbx-mon
+ * By typefo <typefo@qq.com>
+ */
+
+class ApiController extends Yaf\Controller_Abstract {
+
+    public function serverAction() {
+        $server = new ServerModel();
+        $response['status'] = 200;
+        $response['message'] = 'success';
+        $response['data'] = $server->getAll();
+        header('Content-type: application/json');
+        echo json_encode($response);
+        return false;
+	}
+
+    public function createAction() {
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $route = new RouteModel();
+            $route->create($request->getPost());
+            $url = 'http://' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . '/route';
+            $response = $this->getResponse();
+            $response->setRedirect($url);
+            $response->response();
+            return false;
+        }
+
+        return true;
+    }
+
+    public function editAction() {
+        $request = $this->getRequest();
+        $route = new RouteModel();
+
+        if ($request->isPost()) {
+            $route->change($request->getQuery('id'), $request->getPost());
+            $url = 'http://' . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . '/route';
+            $response = $this->getResponse();
+            $response->setRedirect($url);
+            $response->response();
+            return false;
+        }
+
+        $response['status'] = 200;
+        $response['message'] = "success";
+        $response['data'] = $route->get($request->getQuery('id'));
+        header('Content-type: application/json');
+        echo json_encode($response);
+        return false;
+    }
+
+    public function deleteAction() {
+        $id = $this->getRequest()->getQuery('id');
+        $route = new RouteModel();
+        $success = $route->delete($id);
+        $response['status'] = $success ? 200 : 400;
+        $response['message'] = $success ? 'success' : 'failed';
+        header('Content-type: application/json');
+        echo json_encode($response);
+        return false;
+    }
+
+    public function syncAction() {
+        $request = $this->getRequest();
+        $system = new SystemModel();
+        if ($system->regenPlan($this->getRequest()->getQuery('id'))) {
+            $response['status'] = 200;
+            $response['message'] = "success";
+            header('Content-type: application/json');
+            echo json_encode($response);
+            return false;
+        }
+
+        $response['status'] = 400;
+        $response['message'] = "sync failed";
+        header('Content-type: application/json');
+        echo json_encode($response);
+        return false;
+    }
+}
+
+
