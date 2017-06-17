9b07055cebca12a8b4db3e906b1cdb16e4d0e4db
typefo <typefo@qq.com>
2017-06-17 12:58:12 +0800

    updated report model

--- www/app/models/Report.php
+++ www/app/models/Report.php
@@ -32,6 +32,8 @@ class ReportModel {
     public function get($server = null, $date = null) {
         $ip = ip2long($server);
         $reply = array();
+        var_dump($this->redis);
+        exit;
         if (($ip !== false) && $this->redis) {
             if ($data != null) {
                 $prefix = $this->prefix . '.' . $date . '.';
