5a5864a2f4abd8954beed4daf6e8d8bfa0d19348
typefo <typefo@qq.com>
2017-06-17 12:45:08 +0800

    updated report model

--- www/app/controllers/Report.php
+++ www/app/controllers/Report.php
@@ -11,6 +11,8 @@ class ReportController extends Yaf\Controller_Abstract {
     public function indexAction() {
     	$report = new ReportModel();
     	$this->getView()->assign("data", $report->getAll());
+    	var_dump($report->getAll())
+    	exit;
         return true;
 	}
 }
